face94a761f2559e6b24403976503b005c010954
Vincenzo de Luca <jsdeluca01@gmail.com>
2022-12-06 19:13:23 -0300

    Adding sections

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ barsContainer.addEventListener('click', ()=>{
     barsContainer.classList.toggle("activeBars")
     nav.classList.toggle('activeNav')
 })
+
+
+//Ppupers activing
+
+const activers = document.querySelectorAll('#portalActiver')
+const wrappers = document.querySelectorAll('#portalWrapper')
+const popupClosers = document.querySelectorAll('#popupCloser')
+const popups = document.querySelectorAll('.popup')
+
+activers.forEach((activer) =>{
+  activer.addEventListener('click', ()=>{
+    console.log(activer)
+    var rWrapper = [...wrappers].filter(el => el.dataset.subject == activer.dataset.subject).map(elem =>{
+      elem.style.display = 'flex'
+    })
+    })
+})
+
+popupClosers.forEach(closer =>{
+  closer.addEventListener('click', ()=>{
+    [...wrappers].map(e => {
+      e.style.display = 'none'
+    })
+  })
+})
